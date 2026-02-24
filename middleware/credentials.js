ab4b4a0108238c4dbc5fa98a31c420e1bce694d7
Sam Anderson <159704859+inNetMedia@users.noreply.github.com>
2026-02-24 15:42:41 +0200

    cors update

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -3,7 +3,9 @@ const whiteList = ['www.innetmedia.com','innetmedia.com','http://127.0.0.1:5500'
 const credentials = (req, res, next) => {
     const origin = req.headers.origin
     if(whiteList.includes(origin)){
-        res.header({ 'Access-Control-Allow-Credentials': true })
+        res.header({ 'Access-Control-Allow-Credentials': true,
+                    'Access-Control-Allow-Origin': true
+         })
     }
     next()
 }
